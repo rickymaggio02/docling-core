b329fe1517378dee73e4408d1559e8c87dfbea4c
rickymaggio02 <riccardo.maggioni@googlemail.com>
2025-05-09 06:25:48 -0500

    add parameter to choose of which pages export the doctags

--- docling_core/types/doc/document.py
+++ docling_core/types/doc/document.py
@@ -3885,6 +3885,7 @@ class DoclingDocument(BaseModel):
         add_table_cell_location: bool = False,
         add_table_cell_text: bool = True,
         minified: bool = False,
+        pages: list[int] = None
     ) -> str:
         r"""Exports the document content to a DocumentToken format.
 
@@ -3903,6 +3904,7 @@ class DoclingDocument(BaseModel):
         :param # table specific flagsadd_table_cell_location: bool
         :param add_table_cell_text: bool:  (Default value = True)
         :param minified: bool:  (Default value = False)
+        :param pages: list[int]: (Default value = None)
         :returns: The content of the document formatted as a DocTags string.
         :rtype: str
         """
@@ -3927,6 +3929,7 @@ class DoclingDocument(BaseModel):
                 add_page_break=add_page_index,
                 add_table_cell_location=add_table_cell_location,
                 add_table_cell_text=add_table_cell_text,
+                pages=pages,
                 mode=(
                     DocTagsParams.Mode.MINIFIED
                     if minified
